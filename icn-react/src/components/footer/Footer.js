76457d9756e888f9e47f0460249dc404c3a79c60
lilly0305 <yhklilly0305@gmail.com>
2022-04-05 11:30:32 +0900

    web footer 완성

--- icn-react/src/components/footer/Footer.js
+++ icn-react/src/components/footer/Footer.js
@@ -1,11 +1,67 @@
 import { Desktop } from "commons/mediaqueries/MediaQuery"
+import { Link } from "react-router-dom";
 import '../scss/footer/footer.scss';
 
 export const Footer = () => {
   return(
     <Desktop>
       <footer className="webFooter">
-        footer
+        <div className="footerTop">
+          <div className="footerLeft">
+            <p>Join our class &#38; <br />improve your skill</p>
+            <p>
+              The best and lagest online tutoring application in Korea. 
+            </p>
+            <p>
+              The most complete online learning solution
+            </p>
+            <Link to='/sign-up'>Sign up now</Link>
+          </div>
+
+          <div className="footerRight">
+            <ul>
+              <li>
+                <span>Lesson</span>
+                <div>Over view</div>
+                <div>Tutor</div>
+                <div>Pricing</div>
+              </li>
+              <li>
+                <span>About Us</span>
+                <div>Business English</div>
+                <div>Education</div>
+                <div>Certified tutors</div>
+                <div>Professional tutors</div>
+              </li>
+
+              <li>
+                <p>
+                  <span>Tutor</span>
+                  <div>Become a tutor</div>
+                </p>
+                <p>
+                  <span>Career</span>
+                  <div>Internship</div>
+                  <div>Career offer</div>
+                </p>
+              </li>
+              <li>
+                <span>FAQ</span>
+                <div>Most asked question</div>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="footerBottom">
+          <header>ICN</header>
+          <p>The most complete online learning solution</p>
+          <div>
+            <span>Terms &#38; Conditions</span>
+            <span>Privacy Policy</span>
+          </div>
+
+        </div>
       </footer>
     </Desktop>
   )
