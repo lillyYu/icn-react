5e6a149b2cecae8d20380b2ad6765e4ffbb80c1c
ryuhk <yhklilly0305@gmail.com>
2022-04-12 10:11:39 +0900

    tutors main done

--- icn-react/src/tutors/components/main/OwnHome.js
+++ icn-react/src/tutors/components/main/OwnHome.js
@@ -3,36 +3,38 @@ import ServiceItem from "commons/items/service-item/ServiceItem"
 const OwnHome = () => {
   return (
     <section className="ourHome">
-      <div>
-        <h2 className="sectionTitle">
-          <span>
-            Teach professional English online <br/>
-            from <strong>your own home</strong>
-          </span>
-        </h2>
-  
-        <article>
-          <div className="serviceItems">
-            <ServiceItem 
-              bg="blue"
-              icon="bxs-calendar-week"
-              header="Flexible"
-              content="Choose your own hours. Work from home on your terms"
-            />
-            <ServiceItem 
-              bg="orange"
-              icon="bxs-network-chart"
-              header="Networking"
-              content="Hone interpersonal skills and cultural awareness along the way."
-            />
-            <ServiceItem 
-              bg="green"
-              icon="bxs-book"
-              header="Hassle-free"
-              content="Don't worry about preparing lesson plans. Our ICN packets do the work for you."
-            />
-          </div>
-        </article>
+      <div className="bgWrap">
+        <div className="webWrap">
+          <h2 className="sectionTitle">
+            <span>
+              Teach professional English online <br/>
+              from <strong>your own home</strong>
+            </span>
+          </h2>
+    
+          <article>
+            <div className="serviceItems">
+              <ServiceItem 
+                bg="blue"
+                icon="bxs-calendar-week"
+                header="Flexible"
+                content="Choose your own hours. Work from home on your terms"
+              />
+              <ServiceItem 
+                bg="orange"
+                icon="bxs-network-chart"
+                header="Networking"
+                content="Hone interpersonal skills and cultural awareness along the way."
+              />
+              <ServiceItem 
+                bg="green"
+                icon="bxs-book"
+                header="Hassle-free"
+                content="Don't worry about preparing lesson plans. Our ICN packets do the work for you."
+              />
+            </div>
+          </article>
+        </div>
       </div>
     </section>
   )
