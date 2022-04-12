712277d4a0b3497136db763202ecb56e524b2e17
ryuhk <yhklilly0305@gmail.com>
2022-04-12 10:31:08 +0900

    privacy police publishing done

--- icn-react/src/tutors/components/subpages/privacy-policy/PrivacyPolicy.js
+++ icn-react/src/tutors/components/subpages/privacy-policy/PrivacyPolicy.js
@@ -5,14 +5,32 @@ const PrivacyPolicy = () => {
     <div className='privacyPolicy'>
       <h1>Privacy Policy</h1>
 
-      <article>
-        <h2>ICN</h2>
-        <time>Last Updated Mar 18, 2022</time>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, corrupti? Magni similique quasi pariatur? Officia earum suscipit dolorem illum rem.
-        </p>
-        
-      </article>
+      <div className='webWrap'>
+        <article>
+          <h2>ICN</h2>
+          <time>Last Updated Mar 18, 2022</time>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, corrupti? Magni similique quasi pariatur? Officia earum suscipit dolorem illum rem.
+          </p>
+  
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque fuga, quaerat vel ipsam quod praesentium provident aperiam animi enim id dicta ad consequuntur at ipsa. Nesciunt voluptates ducimus dicta, eveniet omnis eum. Nisi, exercitationem molestias rem fugit quos debitis nulla!
+          </p>
+          
+        </article>
+  
+        <article>
+          <h3>Notice to California Residents</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, corrupti? Magni similique quasi pariatur? Officia earum suscipit dolorem illum rem.
+          </p>
+  
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque fuga, quaerat vel ipsam quod praesentium provident aperiam animi enim id dicta ad consequuntur at ipsa. Nesciunt voluptates ducimus dicta, eveniet omnis eum. Nisi, exercitationem molestias rem fugit quos debitis nulla!
+          </p>
+          
+        </article>
+      </div>
     </div>
   )
 }
