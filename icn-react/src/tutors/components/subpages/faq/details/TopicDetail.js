2a49d228c4f71d72ff25758b5db87a7eb13077bf
lilly0305 <yhklilly0305@gmail.com>
2022-04-13 19:00:25 +0900

    faq detail media query done

--- icn-react/src/tutors/components/subpages/faq/details/TopicDetail.js
+++ icn-react/src/tutors/components/subpages/faq/details/TopicDetail.js
@@ -1,4 +1,4 @@
-import { Desktop } from 'commons/mediaqueries/MediaQuery'
+import { Desktop, Mobile } from 'commons/mediaqueries/MediaQuery'
 import React, { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import 'commons/scss/items/details/topic-detail.scss';
@@ -34,10 +34,16 @@ const TopicDetail = () => {
 
   const selectMenuHandler = (index) => {
     setCurrentTab(index);
+    console.log(index)
   };
 
+  const handleChange = (e) => {
+    console.log(e.target.value);
+	};
+
   return (
-    <Desktop>
+    <>
+      <Desktop>
         <div className='topicDetail'>
           <div className='topicDetailWrap'>
             <div className='breadcrumbs'>
@@ -70,14 +76,14 @@ const TopicDetail = () => {
                   })}
                 </ul>
               </aside>
-
+  
               <main>
                 <header>
                   {faqTabs[currentTab].question}
                 </header>
-
+  
                 <time>2 days ago</time>
-
+  
                 <section>
                   <p>
                     {faqTabs[currentTab].answer}
@@ -100,17 +106,93 @@ const TopicDetail = () => {
                     })}
                   </ul>
                 </section>
-
+  
                 <section>
                   <h2>Have more question?</h2>
                   <Link to='/'>Submit a request</Link>
                 </section>
-
+  
               </main>
             </article>
           </div>
         </div>  
       </Desktop>
+  
+      <Mobile>
+        <div className='topicDetail'>
+          <div className='topicDetailWrap'>
+            <div className='breadcrumbs'>
+              <Link to="/tutors/faq" className='mobileDisplayNone'>
+                <i className='bx bx-chevron-left'></i>
+                <span>Back</span>
+              </Link>
+              <nav>
+                <i className='bx bx-home-alt-2'></i>
+                <span>FAQ</span>
+                <i className='bx bx-chevron-right'></i>
+                <span>{topicName}</span>
+              </nav>
+            </div>
+
+            <article>
+              <aside>
+                <h1>{topicName}</h1>
+                
+                <select onChange={handleChange}>
+                  {faqTabs.map((faqTab, index)=>{
+                    return (
+                      <option
+                        key={index}
+                        onClick={()=> selectMenuHandler(index)}
+                      >
+                        {faqTab.question}
+                      </option>
+                    )
+                  })}
+                </select>
+              </aside>
+
+              <main>
+                <header>
+                  {faqTabs[currentTab].question}
+                </header>
+  
+                <time>2 days ago</time>
+  
+                <section>
+                  <p>
+                    {faqTabs[currentTab].answer}
+                  </p>
+                </section>
+                
+                <section className="relatedQuestions">
+                  <h2>Related questions</h2>
+                  <ul>
+                  {faqTabs.map((faqTab, index)=>{
+                    return (
+                      <li
+                        key={index}
+                        className={currentTab === index ? "active" : ""}
+                        onClick={()=> selectMenuHandler(index)}
+                      >
+                        {faqTab.question}
+                      </li>
+                      )
+                    })}
+                  </ul>
+                </section>
+  
+                <section>
+                  <h2>Have more question?</h2>
+                  <Link to='/'>Submit a request</Link>
+                </section>
+  
+              </main>
+            </article>
+          </div>
+        </div>
+      </Mobile>
+    </>
   )
 }
 
