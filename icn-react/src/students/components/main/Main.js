2db088ed22fe47ebe457cc41b17b61d8fb628263
ryuhk <yhklilly0305@gmail.com>
2022-04-14 14:45:32 +0900

    students main publishing:

--- icn-react/src/students/components/main/Main.js
+++ icn-react/src/students/components/main/Main.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import 'students/components/scss/main/main.scss';
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='studentMain'>
+      <div className='studentMainWrap webWrap '>
+        <aside>
+          <ul>
+            <li>
+              <i className='bx bx-time-five blue' />
+              <p>Lessons in 24 hours</p>
+              <span className='blue'>0</span>
+            </li>
+          </ul>
+        </aside>
+
+        <main>
+
+        </main>
+      </div>
+    </div>
   )
 }
 
