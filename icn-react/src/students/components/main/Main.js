351593cdbd26bb83673ff2bf0d6f26a06aa8f33b
ryuhk <yhklilly0305@gmail.com>
2022-04-14 16:40:11 +0900

    students main publishing중

--- icn-react/src/students/components/main/Main.js
+++ icn-react/src/students/components/main/Main.js
@@ -6,10 +6,44 @@ const Main = () => {
       <div className='studentMainWrap webWrap '>
         <aside>
           <ul>
+            <li className='blue'>
+              <div>
+                <i className='bx bx-time-five' />
+                <p>Lessons in 24 hours</p>
+              </div>
+              <span>12</span>
+            </li>
+            <li className='red'>
+              <div>
+                <i className='bx bx-calendar-exclamation'></i>
+                <p>Upcoming Lessons</p>
+              </div>
+              <span>21</span>
+            </li>
+            <li className='green'>
+              <div>
+                <i className='bx bx-notepad'></i>
+                <p>Lessons to review</p>
+              </div>
+              <span>0</span>
+            </li>
             <li>
-              <i className='bx bx-time-five blue' />
-              <p>Lessons in 24 hours</p>
-              <span className='blue'>0</span>
+              <div>
+                <i class='bx bx-calendar-plus'></i>
+                <p>Book class</p>
+              </div>
+            </li>
+            <li>
+              <div>
+                <i class='bx bx-user-plus'></i>
+                <p>Invite friends</p>
+              </div>
+            </li>
+            <li>
+              <div>
+                <i class='bx bx-cart-add' ></i>
+                <p>Buy credit</p>
+              </div>
             </li>
           </ul>
         </aside>
