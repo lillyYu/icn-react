aa500b5ec61555010eae45199f750a96262f8b08
ryuhk <yhklilly0305@gmail.com>
2022-04-14 12:50:01 +0900

    commit before publishing students after login page

--- icn-react/src/landing/components/main/NewTutors.js
+++ icn-react/src/landing/components/main/NewTutors.js
@@ -2,7 +2,7 @@ import { TutorItem } from "commons/items/tutor-item/TutorItem";
 import { Link } from "react-router-dom";
 import Slider from "react-slick/lib/slider";
 import tutors from 'commons/items/tutor-item/tutors.json';
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export const NewTutors = (props) => {
   // const [businessOn, setBusinessOn] = useState('');
