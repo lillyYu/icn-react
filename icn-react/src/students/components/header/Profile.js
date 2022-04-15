645846d6500d2d339b5979e724aa834621545588
lilly0305 <yhklilly0305@gmail.com>
2022-04-15 10:44:14 +0900

    students header 완성

--- icn-react/src/students/components/header/Profile.js
+++ icn-react/src/students/components/header/Profile.js
@@ -1,22 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import profileImage from 'commons/images/profile-sample.jpg';
 import { Link } from 'react-router-dom';
 
 const Profile = () => {
+  const [modalOn, setModalOn] = useState(false);
+  const [arrowToggle, setArrowToggle] = useState(false);
+
+  const handleModalToggle =() => {
+    setModalOn(!modalOn)
+    setArrowToggle(!arrowToggle)
+  }
+
   return (
     <div className='profileStyling'>
       <Link to="/">Buy now</Link>
-      <ul>
+      <ul onClick={handleModalToggle}>
         <li>
           <figure>
             <img src={profileImage} alt="profile sample" />
           </figure>
         </li>
         <li>Angelia</li>
-        <li>
+        <li className={arrowToggle ? "active" : null}>
           <i className='bx bx-chevron-down'></i>
         </li>
       </ul>
+
+      {
+        modalOn &&
+          <div className='profileModal'>
+            <div>
+              <header>
+                Learning
+              </header>
+              <Link to="/">My class</Link>
+              <Link to="/">Review</Link>
+            </div>
+
+            <div>
+              <header>
+                Account
+              </header>
+              <Link to="/">My account</Link>
+              <Link to="/">Point</Link>
+              <Link to="/">Notification</Link>
+            </div>
+
+            <Link to="/">Log out</Link>
+          </div>
+      }
+      
     </div>
   )
 }
